08ff104b58bb9c3f1be209ab443f82a7e175ecdb
xchen <xchen601@gmail.com>
2023-02-19 15:12:03 -0500

    added jsx for navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
 import './App.css';
+import { Routes, Route, Outlet, Link } from "react-router-dom";
 
-function App() {
+export default function App() {
   return (
-    <div>123test321</div>
+    <div className="App">
+      <h1>Welcome to Project One!</h1>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+
+          <Route path="*" element={<NoMatch />} />
+        </Route>
+      </Routes>
+    </div>
   );
 }
 
-export default App;
+function Layout() {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>Places</li>
+          <li>Food</li>
+          <li>Attractions</li>
+          <li>Map</li>
+        </ul>
+      </nav>
+      <Outlet />
+    </div>
+  );
+}
+
+function NoMatch() {
+  return (
+    <div>
+      <h2>Nothing to see here!</h2>
+      <p>
+        <Link to="/">Go to the home page</Link>
+      </p>
+    </div>
+  );
+}
