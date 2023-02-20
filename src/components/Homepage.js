51e1bf452281b333587267c2275664417de43c54
xchen <xchen601@gmail.com>
2023-02-20 03:50:39 -0500

    added grid for homepage displaying the different navbar items

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,7 +1,22 @@
+import './css/Homepage.css'
 function Homepage() {
     return (
-      <div className="content">
-        This is the homepage
+      <div className="homepage">
+        <div className="grid-container">
+            <button className='grid-item'>
+                Lodging
+            </button>
+            <button className='grid-item'>
+                Restaurants
+            </button>
+            <button className='grid-item'>
+                Attractions
+            </button>
+            <button className='grid-item'>
+                Map
+            </button>
+            
+        </div>
       </div>
     );
   }
