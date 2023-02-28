8d135b1806b028d4b3d7299ded5763db69bca26c
ZurielGarciaHuerta <zurgarhu@hotmail.com>
2023-02-28 03:47:04 -0500

    Sucesfully managed to make it take links for images, need to figure out width and height restrictions and fortnite maps itself

--- src/components/Map.js
+++ src/components/Map.js
@@ -18,42 +18,28 @@ function Map() {
   const slidesData = [
     {
       id: 1,
+      src: 'https://www.rd.com/wp-content/uploads/2018/02/30_Adorable-Puppy-Pictures-that-Will-Make-You-Melt_124167640_YamabikaY.jpg?fit=700,467',
       title: 'repellendus id ullam',
       label: 'Dolorem officiis temporibus.'
     }, {
       id: 2,
+      src: 'https://images.unsplash.com/photo-1600804340584-c7db2eacf0bf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8cHVwcHl8ZW58MHx8MHx8&w=1000&q=80',
       title: 'excepturi consequatur est',
       label: 'Officia non provident dolor esse et neque.'
-    }, {
-      id: 3,
-      title: 'eius doloribus blanditiis',
-      label: 'Ut recusandae vel vitae molestiae id soluta.'
-    }, {
-      id: 4,
-      title: 'nihil voluptates delectus',
-      label: 'Qui vel consequatur recusandae illo repellendus.'
-    }, {
-      id: 5,
-      title: 'nemo dolorem necessitatibus',
-      label: 'Placeat odit velit itaque voluptatem.'
-    }, {
-      id: 6,
-      title: 'dolorem quibusdam quasi',
-      label: 'Adipisci officiis repudiandae.'
-    },
+    }, 
   ];
 
   return (
     <div className="content">
-      This page will show the fortnite map
+      This page will show the fortnite mapddd
 
       <Slider {...settings}>
         {slidesData.map((slide) =>
- <div className="slick-slide" key={slide.id}>
+         <div className="slick-slide" key={slide.id}>
               <h2 className="slick-slide-title">{slide.title}</h2>
-              <img className="slick-slide-image" src={`https://picsum.photos/800/400?img=${slide.id}`} />
+              <img className="slick-slide-image" src={slide.src} alt={slide.title} />
               <label className="slick-slide-label">{slide.label}</label>
-            </div>
+          </div>
         )}
       </Slider>
 
