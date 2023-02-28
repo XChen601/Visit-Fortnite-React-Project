fe88df81ced936710742539505f628f219899c2d
ZurielGarciaHuerta <zurgarhu@hotmail.com>
2023-02-28 03:29:53 -0500

    Added React Slick Carrousel and Slick-Caroussel as dependencies; Also have a working protype for map, need to add Fortnite map images and style the CSS of it

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,9 +1,64 @@
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+import './css/Cards.css'
+import React from 'react'
+ 
 function Map() {
-    return (
-      <div className="content">
-        This page will show the fortnite map
-      </div>
-    );
-  }
-  
-  export default Map;
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+
+  };
+
+
+  const slidesData = [
+    {
+      id: 1,
+      title: 'repellendus id ullam',
+      label: 'Dolorem officiis temporibus.'
+    }, {
+      id: 2,
+      title: 'excepturi consequatur est',
+      label: 'Officia non provident dolor esse et neque.'
+    }, {
+      id: 3,
+      title: 'eius doloribus blanditiis',
+      label: 'Ut recusandae vel vitae molestiae id soluta.'
+    }, {
+      id: 4,
+      title: 'nihil voluptates delectus',
+      label: 'Qui vel consequatur recusandae illo repellendus.'
+    }, {
+      id: 5,
+      title: 'nemo dolorem necessitatibus',
+      label: 'Placeat odit velit itaque voluptatem.'
+    }, {
+      id: 6,
+      title: 'dolorem quibusdam quasi',
+      label: 'Adipisci officiis repudiandae.'
+    },
+  ];
+
+  return (
+    <div className="content">
+      This page will show the fortnite map
+
+      <Slider {...settings}>
+        {slidesData.map((slide) =>
+ <div className="slick-slide" key={slide.id}>
+              <h2 className="slick-slide-title">{slide.title}</h2>
+              <img className="slick-slide-image" src={`https://picsum.photos/800/400?img=${slide.id}`} />
+              <label className="slick-slide-label">{slide.label}</label>
+            </div>
+        )}
+      </Slider>
+
+    </div>
+  );
+}
+
+export default Map;
